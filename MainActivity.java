190f1044c3e83ca56f06fc3d7112ff74f2ec3b31
Richard Leosala <chardeefaye@gmail.com>
2018-12-04 00:58:47 +0800

    Inital Commit

--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,24 @@
 package ph.edu.addu.richardleosala.concon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnGameStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnGameStart = findViewById(R.id.btnGameStart);
+        btnGameStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, gameStart.class);
+                startActivity(intent);
+            }
+        });
     }
 }
